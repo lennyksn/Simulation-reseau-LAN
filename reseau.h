671c8013b2f3c2ebeaac4ae5bb8d16e31bdf5c40
Amandine MIranda <amandine.miranda@unistra.fr>
2026-05-22 17:25:11 +0200

    Correction de deux trois trucs pour que ça compile au moins avec gcc -o

--- reseau.h
+++ reseau.h
@@ -8,27 +8,31 @@ typedef uint8_t mac[6];
 
 typedef uint8_t ipv4[4];
 
-typedef struct station{
+typedef struct station
+{
 	mac addr_mac;
 	ipv4 addr_ipv4;
 } station;
 
-typedef struct switch{
+typedef struct EntreeTable
+{
+        mac addr_mac;
+        size_t port;
+} EntreeTable;
+
+typedef struct TableCommutation
+{
+        EntreeTable entree[MAX_ENTREES];
+        size_t nb_entrees;
+} TableCommutation;
+
+typedef struct commutateur
+{
 	mac addr_mac;
 	size_t nb_ports;
 	size_t priorite_stp;
 	TableCommutation table_comm;
-} switch;
-
-typedef struct {
-	mac addr_mac;
-	size_t port;
-}EntreeTable;
-
-typedef struct {
-	EntreeTable entree[MAX_ENTREES];
-	size_t nb_entrees;
-}TableCommutation;
+} commutateur;
 
 
 void afficher_ipv4(ipv4 *addr);
@@ -37,4 +41,4 @@ void afficher_mac(mac *addr);
 
 void afficher_station(station *st);
 
-void afficher_switch(switch *sw);
+void afficher_switch(commutateur *sw);
