b0ac30d861afeb8c5c0be98a2adf66cd3c427dc2
Amandine MIranda <amandine.miranda@unistra.fr>
2026-05-27 15:11:49 +0200

    J'ai enfin réussi à coder afficher_mac, c'était long et fastidieux, la chaleur me fait pas du bien

--- reseau.c
+++ reseau.c
@@ -18,6 +18,10 @@ void afficher_ipv4(ipv4 *addr)
 
 }
 
+void afficher_mac(mac *addr){
+    printf("%02x:%02x:%02x:%02x:%02x:%02x\n", (*addr)[0],(*addr)[1],(*addr)[2],(*addr)[3],(*addr)[4],(*addr)[5]);
+}
+
 void afficher_station(station *st)
 {
 
