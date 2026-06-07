b94ac39a4b288f34a4cf64afacd91d19fc819a2c
Jason-Scott Bebey Bena <jason-scott.bebey-bena@etu.unistra.fr>
2026-06-07 17:35:07 +0200

    utilisation des fonctions afficher_switch et afficher_station

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "reseau.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -9,6 +10,7 @@ int main()
     int score[2] = {0};
     size_t ports;
     size_t priorite;
+    size_t id = 1;
     uint8_t mmac[6] = {0};
     uint8_t iipv4[4] = {0};
     char ligne[128];
@@ -31,28 +33,58 @@ int main()
             // format : 2;XX:XX:XX:XX:XX:XX;nb_ports;priorite
             if (sscanf(ligne, "%*d;""%hhx:%hhx:%hhx:%hhx:%hhx:%hhx;""%ld;""%ld", &mmac[0], &mmac[1], &mmac[2], &mmac[3], &mmac[4], &mmac[5], &ports, &priorite) == 8) // ✅ mmac[5] ajouté                                         // ✅ & ajouté
             {
+                sw* witch = malloc(sizeof(sw));
+
                 mac addr_mac = {mmac[0], mmac[1], mmac[2], mmac[3], mmac[4], mmac[5]};
+                memcpy(witch->addr_mac, addr_mac, sizeof(mac));
+
+                witch->id = id;
+                witch->nb_ports = ports;
+                witch->priorite_stp = priorite;
+                
+                afficher_switch(witch);
+
+                /*printf("Id du switch : %ld\n", witch->id);
                 printf("Adresse MAC : ");
-                afficher_mac(&addr_mac);
-                printf("Nombre de ports : %ld\n", ports);
-                printf("Priorité : %ld\n", priorite);
-                printf("\n");
+                afficher_mac(&witch->addr_mac);
+                printf("Nombre de ports : %ld\n", witch->nb_ports);
+                printf("Priorité : %ld\n", witch->priorite_stp);
+                printf("\n");*/
+                id++;
+
+                free(witch);
             }
         } while (fgets(ligne, sizeof(ligne), fichier) != NULL && ligne[0] == '2');
+
+        id = 1;
 
         do {
             if (ligne[0] != '1') break; // sécurité
             // format : 1;XX:XX:XX:XX:XX:XX;ip
             if (sscanf(ligne,"%*d;""%hhx:%hhx:%hhx:%hhx:%hhx:%hhx;""%hhu.%hhu.%hhu.%hhu", &mmac[0], &mmac[1], &mmac[2], &mmac[3], &mmac[4], &mmac[5], &iipv4[0], &iipv4[1], &iipv4[2], &iipv4[3]) == 10)
             {
-                mac addr_mc = {mmac[0], mmac[1], mmac[2], mmac[3], mmac[4], mmac[5]};
-                printf("Adresse MAC : ");
-                afficher_mac(&addr_mc);
+                station* st = malloc(sizeof(station));
+
+                mac addr_mac = {mmac[0], mmac[1], mmac[2], mmac[3], mmac[4], mmac[5]};
+                memcpy(st->addr_mac, addr_mac, sizeof(mac));
+
+                st->id = id;
 
                 ipv4 addr_ipv4 = {iipv4[0], iipv4[1], iipv4[2], iipv4[3]};
+                memcpy(st->addr_ipv4, addr_ipv4, sizeof(ipv4));
+
+                afficher_station(st);
+
+                /*printf("Adresse MAC : ");
+                afficher_mac(&addr_mc);
+
+                
                 printf("Adresse IP : ");
                 afficher_ipv4(&addr_ipv4);
-                printf("\n");
+                printf("\n");*/
+                id++;
+                
+                free(st);
             }
         } while (fgets(ligne, sizeof(ligne), fichier) != NULL && ligne[0] == '1');
 
